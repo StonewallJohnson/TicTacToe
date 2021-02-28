feda7500c260c95a3153ebc68072cbcb14729465
Landon Johnson <landon.johnson@utexas.edu>
2021-02-28 11:55:51 -0600

    added printBoard

--- board.py
+++ board.py
@@ -1,12 +1,32 @@
 class Board:
     def __init__(self):
-        self.boardContainer = [[" ", "|", " ", "|", " "],
-                               [" ", "|", " ", "|", " "],
-                               [" ", "|", " ", "|", " "]]
+        self.boardCon = [" ", " ", " ",
+                         " ", " ", " ",
+                         " ", " ", " "]
+        self.rows = self.__getRows()
+        self.columns = self.__getColumns()
+        self.diagonals = self.__getDiagonals()
         
     def printBoard(self):
-        for row in self.boardContainer:
-            for val in row:
-                print(val, end="")
-            print("\n______")
-        print()
+        print("   |   |")
+        print(" " + self.boardCon[0] + " | " + self.boardCon[1] + " | " + self.boardCon[2])
+        print("   |   |")
+        print("-----------")
+        print("   |   |")
+        print(" " + self.boardCon[3] + " | " + self.boardCon[4] + " | " + self.boardCon[5])
+        print("   |   |")
+        print("-----------")
+        print("   |   |")
+        print(" " + self.boardCon[6] + " | " + self.boardCon[7] + " | " + self.boardCon[8])
+        print("   |   |")
+
+    
+    
+    def __getRows(self):
+        pass
+
+    def __getColumns(self):
+        pass
+
+    def __getDiagonals(self):
+        pass
